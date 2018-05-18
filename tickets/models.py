69e060c358583d65b688e888a56f47fd79871554
Matheus R <matheusrf96@gmail.com>
2018-05-18 16:32:10 -0300

    Correçõa models

--- tickets/models.py
+++ tickets/models.py
@@ -17,15 +17,3 @@ class Ticket(models.Model):
 
     def __unicode__(self):
         return u"%s - %s" % (self.titulo, self.autor)
-
-    '''
-    class Meta:
-        verbose_name = u"Processo de Matrícula - Série"
-        verbose_name_plural = u"Processos de Matrículas - Séries"
-        unique_together = ('ProcessoMatricula', 'Serie')
-        ordering = ['ProcessoMatricula', 'Serie']
-
-    def __unicode__(self):
-        return u"%s - %s" % (self.ProcessoMatricula.AnoLetivo, self.Serie.Descricao)
-
-    '''
