ef7c7c05bd390430b99d4ae80afdadfb96a2f8af
Matheus <matheusrf96@gmail.com>
2018-08-13 17:29:12 -0300

    Mudança nas portas utilizadas

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
 FROM python:3
 ENV PYTHONUNBUFFERED 1
+
 RUN mkdir /code
 WORKDIR /code
+
 ADD src/requirements.txt /code/
 RUN pip install --upgrade pip
 RUN pip install -r requirements.txt
 ADD ./src/ /code/
+
+EXPOSE 8020
--- src/helpdesk_django/settings.py
+++ src/helpdesk_django/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -23,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
