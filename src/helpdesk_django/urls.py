0cfdd1bcf66787cfd65e2dd827fd547b18441702
Matheus <matheusrf96@gmail.com>
2018-08-14 14:40:21 -0300

    Nova inicialização do container

--- src/helpdesk_django/urls.py
+++ src/helpdesk_django/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic.base import RedirectView
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('', RedirectView.as_view(url='tickets/', permanent=False), name='index'),
-    path('admin/', admin.site.urls),
     path('tickets/', include('tickets.urls')),
-]
+    path('admin/', admin.site.urls),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
