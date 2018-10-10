26c7cb2de328458d6883c9d86fe1ad126a2fb9ae
Matheus Rodrigues <matheusrf96@gmail.com>
2018-10-10 15:43:35 -0300

    Delete docker_init.sh

--- src/tickets/templates/tickets/docker_init.sh
+++
@@ -1,7 +0,0 @@
-# !/bin/bash
-
-echo "===== Iniciando Helpdesk... ====="
-cd /home/matheus/helpdesk_django && sudo docker-compose restart
-
-echo "===== Iniciando Intranet... ====="
-cd /home/matheus/intranet && sudo docker-compose restart
